9947f800649c97392ddbac4140c8c10a1c82e00b
alreadysons <alreadysons@gmail.com>
2023-05-23 10:39:43 +0900

    wether_style

--- TestWether/App.js
+++ TestWether/App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,StyleSheet,Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={Style.container}>
+        <StatusBar style='light'/>
+        <View style={Style.city}>
+          <Text style={Style.cityName}>WONJU</Text>
+        </View>
+        <View style = {Style.wether}>
+          <View style = {Style.day}>
+          <Text stytle = {Style.temp}>27</Text>
+          <Text style = {Style.desp}>sunny</Text>
+          </View>
+        </View>
+      </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Style = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor : "khaki",
   },
+  city : {
+    flex : 1,
+    justifyContent : "center",
+    alignItems : "center",
+  },
+  cityName : {
+    fontSize : 68,
+    color : "black",
+    fontWeight : "500",
+  },  
+  wether : {
+    flex : 3,
+  },
+  day : {
+    flex : 1,
+    backgroundColor : "red", 
+    justifyContent : "center",  
+    alignItems : "center",
+  },
+  temp : {
+    fontSize : 158,
+  },
+  desp : {
+    fontSize : 100,
+  }
 });
